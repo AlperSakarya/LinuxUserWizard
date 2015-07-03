654a40bbcbd0eafcedd0edaab331fd07acea2dc9
sehpatr <alper@turquoiseis.com>
2015-07-03 02:42:18 -0400

    Improvements

--- linux-user-wizard.sh
+++ linux-user-wizard.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-#set -x 
+#set -x
 
-if ! [ $(id -u) = 0 ]; then
-    echo "Tou need to have root privileges to run this script
+if ! [ $(id -u) = 0 ]
+    then
+        echo "Tou need to have root privileges to run this script
 Please try again, this time using 'sudo'. Exiting."
-    exit
+        exit
 fi
 
 
@@ -13,11 +14,12 @@ function exiting {
     read exitanswer
     if [ "$exitanswer" = "y" ] || [ "$exitanswer" = "Y" ]
         then
-         bash ./linux-user-wizard.sh
+            bash ./linux-user-wizard.sh
     elif [ "$exitanswer" = "n" ] || [ "$exitanswer" = "N" ]
         then
-    echo "GOOD BYE"
-    else echo "Wrong option, please enter 'Y' or 'N'"
+            echo "GOOD BYE"
+    else
+        echo "Wrong option, please enter 'Y' or 'N'"
     exiting
     fi
 }
@@ -25,6 +27,16 @@ function exiting {
 function wrongoption {
     echo "Wrong option, please enter 'Y' or 'N'"
     exiting
+
+}
+
+
+function keypairgen {
+    ssh-keygen -t rsa
+    mv /root/.ssh/id_rsa* /home/$luwuser
+    cat /home/$luwuser/id_rsa.pub >> /home/$luwuser/.ssh/authorized_keys
+    chown -R $luwuser /home/$luwuser
+    chmod 600 /home/$luwuser/.ssh/authorized_keys
 
 }
 
@@ -49,108 +61,99 @@ echo       "   Select a number and hit 'Enter' "
 
 read answer
 
-if [ "$answer" = "1" ]
+if [ "$answer" = "1" ] ### OPTION 1 START
     then
-    echo "Please enter a username"
-    read luwuser
-    
-    if [ -d /home/$luwuser ]
-        then echo "Homefolder exists do you want to overwrite it? (y/n)"
-        read homefolderanswer
-        if [ "$homefolderanswer" = "y" ] || [ "$homefolderanswer" = "Y" ]
-            then
-            rm -rf /home/$luwuser
-            useradd $luwuser -s /bin/bash
-            mkdir /home/$luwuser/.ssh
-            touch /home/$luwuser/.ssh/authorized_keys
-            ssh-keygen -t rsa
-            mv /root/.ssh/id_rsa* /home/$luwuser
-            cat /home/$luwuser/id_rsa.pub >> /home/$luwuser/.ssh/authorized_keys
-            chown -R $luwuser /home/$luwuser
-            chmod 600 /home/$luwuser/.ssh/authorized_keys
-            exiting
-        else
-	    echo "Not creating the user since you want to keep the homefolder"
-            echo "Leaving homefolder intact"
-            echo "Do you still want to create SSH key and put in user's home folder? (y/n)"
-	    read keyans
-	    if [ "$keyans" = "y" ] || [ "$keyans" = "Y" ]
-	        then
-	    	ssh-keygen -t rsa
-            	mv /root/.ssh/id_rsa* /home/$luwuser
-            	cat /home/$luwuser/id_rsa.pub >> /home/$luwuser/.ssh/authorized_keys
-            	chown -R $luwuser /home/$luwuser
-            	chmod 600 /home/$luwuser/.ssh/authorized_keys
-	    fi
-	   
-	   	exiting
-         fi
-   fi
-fi
+        echo "Please enter a username"
+        read luwuser
+        if [ -d /home/$luwuser ]
+            then echo "Homefolder exists do you want to overwrite it? (y/n)"
+            read homefolderanswer
+            if [ "$homefolderanswer" = "y" ] || [ "$homefolderanswer" = "Y" ]
+                then
+                    rm -rf /home/$luwuser
+                    useradd $luwuser -s /bin/bash
+                    mkdir /home/$luwuser/.ssh
+                    touch /home/$luwuser/.ssh/authorized_keys
+                    keypairgen
+                    exiting
+            else
+                echo "Not creating the user since you want to keep the homefolder"
+                echo "Leaving homefolder intact"
+                echo "Do you still want to create SSH key and put in user's home folder? (y/n)"
+                read keyans
+                if [ "$keyans" = "y" ] || [ "$keyans" = "Y" ]
+                    then
+                    keypairgen
+                fi
+	   	    exiting
+            fi
+        fi
+
 
     if [ ! -d /home/$luwuser ]
         then
-	    useradd $luwuser -s /bin/bash 
+    	    useradd $luwuser -s /bin/bash
+    	    if [ ! -d /home/$luwuser ]
+    	        then
+    	            mkdir /home/$luwuser # check due ubuntu does not create home folder on user creation
+    	    fi
             mkdir /home/$luwuser/.ssh
-	    touch /home/$luwuser/.ssh/authorized_keys
-            ssh-keygen -t rsa
-            mv /root/.ssh/id_rsa* /home/$luwuser
-            cat /home/$luwuser/id_rsa.pub >> /home/$luwuser/.ssh/authorized_keys
-            chown -R $luwuser /home/$luwuser
-            chmod 600 /home/$luwuser/.ssh/authorized_keys
+	        touch /home/$luwuser/.ssh/authorized_keys
+            keypairgen
             exiting    
 
-fi
-
-#if [ ! -f /home/$luwuser/.ssh/authorized_keys ]
-#   then
-#       touch /home/$luwuser/.ssh/authorized_keys
-#fi
+    fi
+fi ### OPTION 1 END
 
 
-
-if [ "$answer" = "2" ]
+if [ "$answer" = "2" ] ### OPTION 2 START
     then
-    echo "Please enter a username"
-    read luwuser
-    userdel -r $luwuser
-    echo "User and homefolder deleted"
-    exiting
-fi
+        echo "Please enter a username to delete"
+        read luwuser
+        if [ -d /home/$luwuser ]
+            then
+                userdel -r $luwuser
+                echo "User and homefolder deleted"
+                exiting
+        else
+            echo "Home folder does not exist"
+            exiting
+        fi
+fi ### OPTION 2 END
 
 
-if [ "$answer" = "3" ]
+if [ "$answer" = "3" ] ### OPTION 3 START
     then
-    echo "not ready yet"
-    exiting
-fi
+        echo "not ready yet"
+        exiting
+fi ### OPTION 3 END
 
 
 if [ "$answer" = "4" ]
     then
-    echo "not ready yet"
-    exiting
+        echo "not ready yet"
+        exiting
 fi
 
 
 if [ "$answer" = "5" ]
     then
-    cat /etc/passwd | grep /bin/bash | less
-    ./linux-user-wizard.sh
+        cat /etc/passwd | grep /bin/bash | less
+        ./linux-user-wizard.sh
 fi
 
 
 if [ "$answer" = "6" ]
     then
-    echo "Please enter the username to view it's private key"
-    read keyviewuser
+        echo "Please enter the username to view it's private key"
+        read keyviewuser
     if [ -f /home/$keyviewuser/id_rsa ]
         then
             cat /home/$keyviewuser/id_rsa | less
             exiting
     else
         echo "Private key is not under" /home/$keyviewuser "or not named id_rsa"
-    exiting
+        exiting
     fi
 fi
 
